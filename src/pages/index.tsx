de9a0097618a3d32d06810835371fe52954864c1
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-12-02 01:15:40 +0900

    add: 軽い予定表作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import type { NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { trpc } from "../utils/trpc";
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-
+import jaLocale from '@fullcalendar/core/locales/ja';
 const Home: NextPage = () => {
   return (
     <>
-      <div>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
-    </div>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          locales={[jaLocale]}         // 追加
+          locale='ja'
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: ''
+          }}
+          events={[
+            { title: 'e1', start: '2022-12-14' },
+            { title: 'e2', start: '2022-12-14', end: '2022-12-27' },
+            { title: 'e3', start: '2022-12-12', end: '2022-12-17' },
+            { title: 'e4', start: '2022-12-1', end: '2022-12-28' },
+            { title: 'e5', start: '2022-12-24', end: '2022-12-29' },
+          ]}
+        />
     </>
   );
 };
