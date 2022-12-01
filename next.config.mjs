bd00076c85f6a03a465009bc873137d3c6b21d2f
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-12-02 01:04:00 +0900

    t3でfullcalenderを使用する

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { env } from "./src/env/server.mjs";
-
+import withTM from "next-transpile-modules";
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -13,12 +13,19 @@ function defineNextConfig(config) {
   return config;
 }
 
-export default defineNextConfig({
-  reactStrictMode: true,
-  swcMinify: true,
-  // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-});
+
+export default withTM([
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/react',
+])(
+  defineNextConfig({
+    reactStrictMode: true,
+    swcMinify: true,
+    // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
+    i18n: {
+      locales: ["en"],
+      defaultLocale: "en",
+    },
+  })
+);
