550197a6ade14b891670eab8c8bf3a99b9c6e5ab
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-12-04 18:33:42 +0900

    add: router作成

---
+++ src/server/trpc/router/schedule.ts
@@ -0,0 +1,33 @@
+// 各スキーマのインポート
+import { createScheduleSchema } from '../.././../schema/schedule';
+import { authedProcedure, t } from "../trpc";
+
+// Routerの作成
+
+export const ScheduleRouter = t.router({
+  // 各プロシージャの作成
+  createSchedule: authedProcedure.input(createScheduleSchema)
+    .mutation(async ({ ctx, input }) => {
+      const schedule = await ctx.prisma.schedule.create({
+        data: {
+          ...input,
+          user: {
+            connect: {
+              id: ctx.session?.user?.id,
+            }
+          }
+        }
+      })
+      return schedule
+    }),
+  getSchedules: t.procedure.query(({ ctx }) => {
+    return ctx.prisma.schedule.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    })
+  }),
+})
