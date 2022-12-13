e60119b0284a5b8aaa914e806e463945e48146f1
RyotaAbe1014 <bm1000bmmm@gmail.com>
2022-12-13 22:34:35 +0900

    add: zustandの処理を追加

--- src/hooks/useMutateSchedule.ts
+++ src/hooks/useMutateSchedule.ts
@@ -1,22 +1,25 @@
 import { trpc } from '../utils/trpc'
+import useStore from '../store'
 
 
 export const useMutateSchedule = () => {
   // キャッシュにアクセスする必要があるのでrpc.useContext()を使用してアクセスできるようにする
   const utils = trpc.useContext();
+  const reset = useStore((state) => state.resetEditedSchedule);
 
   const createScheduleMutation = trpc.schedule.createSchedule.useMutation({
     // 成功した時の後処理
-    // resはcreateTaskの返り値を受け取ってキャッシュの配列に追加したいので
+    // resはcreateScheduleの返り値を受け取ってキャッシュの配列に追加したいので
     onSuccess: (res) => {
       // キャッシュを取得
-      const previousSchedules = utils.schedule.getSchedules.getData()
+      const previousSchedules = utils.schedule.getSchedules.getData();
       if (previousSchedules) {
         // キャッシュに追加したデータを追加
-        utils.schedule.getSchedules.setData({ res, ...previousSchedules })
+        utils.schedule.getSchedules.setData({ res, ...previousSchedules });
         window.location.reload();
       }
+      reset();
     },
-  })
+  });
   return { createScheduleMutation }
 }
